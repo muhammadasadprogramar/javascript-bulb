603dcf875d4454dbd7111f746ba804b724780487
muhammadasadprogramar <muhammadasad852233@gmail.com>
2024-05-23 16:46:01 +0500

    javascript bulb

---
+++ app.js
@@ -0,0 +1,9 @@
+function onn() {
+  var blub = document.getElementById("bulb");
+  bulb.src = "images/on.gif";
+}
+
+function off() {
+  var bulb = document.getElementById("bulb");
+  bulb.src = "images/off.gif";
+}
